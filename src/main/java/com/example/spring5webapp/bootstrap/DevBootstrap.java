b349723cc8e2ee732b89099b500a74266c5b62e5
Akhil K J <akhil.janardhanan@litmus7.com>
2018-06-12 18:45:07 +0530

    Created a publisher class and interface for repository

--- src/main/java/com/example/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/com/example/spring5webapp/bootstrap/DevBootstrap.java
@@ -6,18 +6,23 @@ import org.springframework.stereotype.Component;
 
 import com.example.spring5webapp.model.Author;
 import com.example.spring5webapp.model.Book;
+import com.example.spring5webapp.model.Publisher;
 import com.example.spring5webapp.repositories.AuthorRepository;
 import com.example.spring5webapp.repositories.BookRepository;
+import com.example.spring5webapp.repositories.PublisherRepository;
 
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
 	private AuthorRepository authorRepository;
 	private BookRepository bookRepository;
+	private PublisherRepository publisherRepository;
 	
-	public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+	public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, 
+			PublisherRepository publisherRepository) {
 		this.authorRepository = authorRepository;
 		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository;
 	}
 	
 	@Override
@@ -28,8 +33,11 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
 	private void initData() {
 		
+		Publisher publisher = new Publisher("Harper Collins", "XXXXXXXXXXX");
+		publisherRepository.save(publisher);
+		
 		Author eric = new Author("Eric", "Evans");
-		Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+		Book ddd = new Book("Domain Driven Design", "1234", publisher);
 		eric.getBooks().add(ddd);
 		ddd.getAuthors().add(eric);
 		
@@ -37,7 +45,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 		bookRepository.save(ddd);
 		
 		Author rod = new Author("rod", "Jhonson");
-		Book noEJB = new Book("J2EE Development Without EJB", "1234567", "Worx");
+		Book noEJB = new Book("J2EE Development Without EJB", "1234567", publisher);
 		rod.getBooks().add(noEJB);
 		noEJB.getAuthors().add(rod);
 		
