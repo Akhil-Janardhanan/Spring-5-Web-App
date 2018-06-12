cf8f675fedfa595397f9061097e58b6b85c0fee4
Akhil K J <akhil.janardhanan@litmus7.com>
2018-06-12 15:02:14 +0530

    Added DevBootstrap to write data to H2.

---
+++ src/main/java/com/example/spring5webapp/bootstrap/DevBootstrap.java
@@ -0,0 +1,48 @@
+package com.example.spring5webapp.bootstrap;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import com.example.spring5webapp.model.Author;
+import com.example.spring5webapp.model.Book;
+import com.example.spring5webapp.repositories.AuthorRepository;
+import com.example.spring5webapp.repositories.BookRepository;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+	private AuthorRepository authorRepository;
+	private BookRepository bookRepository;
+	
+	public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+		this.authorRepository = authorRepository;
+		this.bookRepository = bookRepository;
+	}
+	
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		
+		initData();
+	}
+
+	private void initData() {
+		
+		Author eric = new Author("Eric", "Evans");
+		Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+		eric.getBooks().add(ddd);
+		ddd.getAuthors().add(eric);
+		
+		authorRepository.save(eric);
+		bookRepository.save(ddd);
+		
+		Author rod = new Author("rod", "Jhonson");
+		Book noEJB = new Book("J2EE Development Without EJB", "1234567", "Worx");
+		rod.getBooks().add(noEJB);
+		noEJB.getAuthors().add(rod);
+		
+		authorRepository.save(rod);
+		bookRepository.save(noEJB);
+	}
+
+}
